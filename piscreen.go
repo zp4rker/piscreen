08654792efc8e5271b0e785b21e576d1268cf5f7
zp4rker <iam@zp4rker.com>
2023-01-26 08:41:10 +1100

    use hex colour

--- piscreen.go
+++ piscreen.go
@@ -24,7 +24,7 @@ func main() {
 	canvas := device.GetFullScreenCanvas()
 
 	draw := gg.NewContext(240, 240)
-	draw.SetRGB(255, 0, 0)
+	draw.SetHexColor("#FF0000")
 	draw.DrawRectangle(0, 0, 240, 240)
 	draw.Fill()
 
