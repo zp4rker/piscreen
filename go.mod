b8c37ee588a8ff12b19d99454aba6d522a9a7fa6
zp4rker <iam@zp4rker.com>
2023-05-02 13:50:01 +1000

    lets try this library

--- go.mod
+++ go.mod
@@ -4,10 +4,13 @@ go 1.19
 
 require (
 	github.com/fogleman/gg v1.3.0
+	github.com/rubiojr/go-pirateaudio v0.0.0-20210122162734-078b8269f3b2
 	github.com/stianeikeland/go-rpio/v4 v4.6.0
+	golang.org/x/image v0.3.0
 )
 
 require (
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
-	golang.org/x/image v0.3.0 // indirect
+	periph.io/x/conn/v3 v3.6.7 // indirect
+	periph.io/x/host/v3 v3.6.7 // indirect
 )
--- piscreen.go
+++ piscreen.go
@@ -1,16 +1,34 @@
 package main
 
 import (
-	"github.com/stianeikeland/go-rpio/v4"
-	"piscreen/spi/impl"
+	"bytes"
+	"github.com/fogleman/gg"
+	"github.com/rubiojr/go-pirateaudio/display"
+	"golang.org/x/image/bmp"
+	"image/color"
 )
 
 func main() {
-	if err := rpio.Open(); err != nil {
+	dsp, err := display.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer dsp.Close()
+
+	context := gg.NewContext(240, 240)
+	context.SetRGB(255, 0, 0)
+	context.DrawRectangle(0, 0, 240, 240)
+	context.Fill()
+
+	buf := new(bytes.Buffer)
+	if err := bmp.Encode(buf, context.Image()); err != nil {
 		panic(err)
 	}
 
-	defer rpio.Close()
+	// Set the screen color to white
+	dsp.FillScreen(color.RGBA{R: 0, G: 0, B: 0, A: 0})
 
-	impl.NewST7789()
+	// Rotate before pushing pixels, so the image appears rotated
+	dsp.Rotate(display.ROTATION_180)
+	dsp.DrawImage(buf)
 }
