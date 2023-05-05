2ce2b2bca1750c55f77b743a67eea1879434531b
zp4rker <iam@zp4rker.com>
2023-05-05 11:27:26 +1000

    didn't actually call clear

--- piscreen.go
+++ piscreen.go
@@ -1,28 +1,22 @@
 package main
 
 import (
-	"bytes"
-	"github.com/fogleman/gg"
-	"golang.org/x/image/bmp"
 	"piscreen/spi/impl"
-	"time"
 )
 
 func main() {
 	disp := impl.NewST7789()
 	defer disp.Close()
 
-	context := gg.NewContext(240, 240)
-	context.SetRGB(255, 0, 0)
-	context.DrawRectangle(0, 0, 240, 240)
-	context.Fill()
+	disp.Clear()
 
-	buf := new(bytes.Buffer)
-	if err := bmp.Encode(buf, context.Image()); err != nil {
-		panic(err)
-	}
-
-	println("Anything happen?")
-	time.Sleep(5 * time.Second)
-	println("How about now?")
+	//context := gg.NewContext(240, 240)
+	//context.SetRGB(255, 0, 0)
+	//context.DrawRectangle(0, 0, 240, 240)
+	//context.Fill()
+	//
+	//buf := new(bytes.Buffer)
+	//if err := bmp.Encode(buf, context.Image()); err != nil {
+	//	panic(err)
+	//}
 }
