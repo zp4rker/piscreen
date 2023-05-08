63c0df2c017fef8214377a06b65a75a38f63c519
zp4rker <iam@zp4rker.com>
2023-05-09 08:08:54 +1000

    lets try drawing text

--- screens/home.go
+++ screens/home.go
@@ -1,8 +1,10 @@
 package screens
 
 import (
+	"fmt"
 	"image"
 	"piscreen/util"
+	"time"
 )
 
 type Home struct{}
@@ -13,6 +15,11 @@ func (s Home) Render() image.Image {
 	context.SetRGB(util.GGColor(0xFF, 0xFF, 0xFF))
 	context.DrawLine(0, 210, 240, 210)
 	context.Stroke()
+
+	if err := context.LoadFontFace("JetBrainsMono.ttf", 17); err != nil {
+		fmt.Printf("Failed to load font!")
+	}
+	context.DrawStringAnchored(time.Now().Format("Mon 2 Jan 2006 15:04"), 120, 215, 0.5, 0)
 
 	return context.Image()
 }
