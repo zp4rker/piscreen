ddeb98886f71d09c8ec10ca737a122800b5d0487
zp4rker <iam@zp4rker.com>
2023-05-09 10:53:43 +1000

    start testing gpio buttons

--- go.mod
+++ go.mod
@@ -5,7 +5,11 @@ go 1.19
 require (
 	github.com/fogleman/gg v1.3.0
 	github.com/stianeikeland/go-rpio/v4 v4.6.0
-	golang.org/x/image v0.3.0
+	periph.io/x/conn/v3 v3.7.0
+	periph.io/x/host/v3 v3.8.2
 )
 
-require github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+require (
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.3.0 // indirect
+)
---
+++ keys/listener.go
@@ -0,0 +1,27 @@
+package keys
+
+import (
+	"periph.io/x/conn/v3/gpio"
+	"periph.io/x/conn/v3/gpio/gpioreg"
+	"periph.io/x/host/v3"
+)
+
+func Listen() {
+	if _, err := host.Init(); err != nil {
+		panic(err)
+	}
+
+	p := gpioreg.ByName("13")
+	if p == nil {
+		panic("failed to register pin")
+	}
+
+	if err := p.In(gpio.PullDown, gpio.FallingEdge); err != nil {
+		panic(err)
+	}
+
+	for {
+		p.WaitForEdge(-1)
+		println("Got press!")
+	}
+}
