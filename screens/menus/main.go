4cfd19c1b957b10551cb7367daf1283c38fec8fe
zp4rker <iam@zp4rker.com>
2023-05-11 06:34:51 +1000

    main menu: create screen

---
+++ screens/menus/main.go
@@ -0,0 +1,34 @@
+package menus
+
+import (
+	"fmt"
+	"image"
+	"piscreen/screens"
+	"piscreen/util"
+)
+
+type Main struct{}
+
+func (s Main) Id() string {
+	return "main_menu"
+}
+
+func (s Main) Render() image.Image {
+	context := screens.BaseScreen(false)
+
+	context.SetRGB(util.GGColor(0xFF, 0xFF, 0xFF))
+	for i := 0; i < 5; i++ {
+		context.DrawRectangle(10, float64(10+i*46), 220, 36)
+		context.Stroke()
+
+		context.DrawStringAnchored(fmt.Sprintf("Button %v", i), 120, float64(10+i*46+18), 0.5, 0.5)
+	}
+
+	return context.Image()
+}
+
+func (s Main) Handle(key string) {
+	if util.DefaultHandle(key) {
+		return
+	}
+}
