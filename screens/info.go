409abe2d16717c4be72d5c60108d7748a867fa44
zp4rker <iam@zp4rker.com>
2023-05-10 17:18:33 +1000

    move footer to base screen

--- screens/info.go
+++ screens/info.go
@@ -12,24 +12,12 @@ import (
 	"piscreen/util"
 	"piscreen/vars"
 	"strings"
-	"time"
 )
 
 type Info struct{}
 
 func (s Info) Render() image.Image {
-	context := Background()
-
-	// Footer
-	context.SetRGB(util.GGColor(0xFF, 0xFF, 0xFF))
-	context.DrawLine(0, 210, 240, 210)
-	context.Stroke()
-	now := time.Now()
-	timeFmt := "Mon 2 Jan 2006 15:04"
-	if now.Unix()%2 == 0 {
-		timeFmt = "Mon 2 Jan 2006 15 04"
-	}
-	context.DrawStringAnchored(now.Format(timeFmt), 120, 215, 0.5, 1.25)
+	context := BaseScreen(true)
 
 	lines := 0
 
--- screens/vars.go
+++ screens/vars.go
@@ -5,6 +5,7 @@ import (
 	"github.com/fogleman/gg"
 	"image"
 	"piscreen/util"
+	"time"
 )
 
 var CurrentScreen Screen = Info{}
@@ -14,7 +15,7 @@ type Screen interface {
 	Handle(key string)
 }
 
-func Background() *gg.Context {
+func BaseScreen(footer bool) *gg.Context {
 	context := gg.NewContext(240, 240)
 
 	context.SetRGB(util.GGColor(0x35, 0x37, 0x39))
@@ -23,6 +24,18 @@ func Background() *gg.Context {
 
 	if err := context.LoadFontFace("JetBrainsMono.ttf", 17); err != nil {
 		fmt.Printf("Failed to load font!")
+	}
+
+	if footer {
+		context.SetRGB(util.GGColor(0xFF, 0xFF, 0xFF))
+		context.DrawLine(0, 210, 240, 210)
+		context.Stroke()
+		now := time.Now()
+		timeFmt := "Mon 2 Jan 2006 15:04"
+		if now.Unix()%2 == 0 {
+			timeFmt = "Mon 2 Jan 2006 15 04"
+		}
+		context.DrawStringAnchored(now.Format(timeFmt), 120, 215, 0.5, 1.25)
 	}
 
 	return context
