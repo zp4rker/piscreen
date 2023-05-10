933500f39eab9a1a82658348ab735f19a685bc48
zp4rker <iam@zp4rker.com>
2023-05-10 15:29:41 +1000

    home screen: add cpu

--- screens/home.go
+++ screens/home.go
@@ -2,6 +2,7 @@ package screens
 
 import (
 	"fmt"
+	"github.com/shirou/gopsutil/v3/cpu"
 	"github.com/shirou/gopsutil/v3/disk"
 	"github.com/shirou/gopsutil/v3/host"
 	"github.com/shirou/gopsutil/v3/mem"
@@ -54,6 +55,12 @@ func (s Home) Render() image.Image {
 				}
 			}
 		}
+	}
+
+	if cpup, err := cpu.Percent(0, false); err == nil {
+		s := fmt.Sprintf("CPU: %.0f%%", cpup[0])
+		context.DrawStringAnchored(s, 5, float64(5+lines*17), 0, 1.25)
+		lines++
 	}
 
 	if vm, err := mem.VirtualMemory(); err == nil {
