593c3a73dcd994f7414036c2c65b9920266daa4a
zp4rker <iam@zp4rker.com>
2023-05-10 11:54:39 +1000

    start populating home screen

--- go.mod
+++ go.mod
@@ -4,12 +4,20 @@ go 1.19
 
 require (
 	github.com/fogleman/gg v1.3.0
+	github.com/shirou/gopsutil/v3 v3.23.4
 	github.com/stianeikeland/go-rpio/v4 v4.6.0
 	periph.io/x/conn/v3 v3.7.0
 	periph.io/x/host/v3 v3.8.2
 )
 
 require (
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/tklauser/go-sysconf v0.3.11 // indirect
+	github.com/tklauser/numcpus v0.6.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
 	golang.org/x/image v0.3.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
 )
--- screens/home.go
+++ screens/home.go
@@ -2,6 +2,7 @@ package screens
 
 import (
 	"fmt"
+	"github.com/shirou/gopsutil/v3/mem"
 	"image"
 	"piscreen/util"
 	"piscreen/vars"
@@ -13,19 +14,23 @@ type Home struct{}
 func (s Home) Render() image.Image {
 	context := Background()
 
-	context.SetRGB(util.GGColor(0xFF, 0xFF, 0xFF))
-	context.DrawLine(0, 210, 240, 210)
-	context.Stroke()
-
 	if err := context.LoadFontFace("JetBrainsMono.ttf", 17); err != nil {
 		fmt.Printf("Failed to load font!")
 	}
+
+	// Footer
+	context.SetRGB(util.GGColor(0xFF, 0xFF, 0xFF))
+	context.DrawLine(0, 210, 240, 210)
+	context.Stroke()
 	now := time.Now()
 	timeFmt := "Mon 2 Jan 2006 15:04"
 	if now.Unix()%2 == 0 {
 		timeFmt = "Mon 2 Jan 2006 15 04"
 	}
 	context.DrawStringAnchored(now.Format(timeFmt), 120, 215, 0.5, 1.25)
+
+	vm, _ := mem.VirtualMemory()
+	context.DrawString(fmt.Sprintf("Memory: %v%", vm.UsedPercent), 10, 10)
 
 	return context.Image()
 }
